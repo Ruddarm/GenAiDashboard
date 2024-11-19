b1be73bbeb2342830d64b2bc2935384633007639
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-19 14:03:18 +0530

    Upadted allwoed origin

--- backend/route/index.js
+++ backend/route/index.js
@@ -4,27 +4,32 @@ const cors = require("cors");
 const DbOperation = require("../Db/MongoOperation");
 const participant = require("../Models/participants");
 
-const allowedOrigins = ['https://gdg-on-bvp-dms.netlify.app/','http://localhost:3000'];
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
-  
-app.use(corsOptions)
 dotenv.config();
 const app = express();
+app.use(corsOptions);
+
 DbOperation();
+
+const allowedOrigins = [
+  "https://gdg-on-bvp-dms.netlify.app/",
+  "http://localhost:3000",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.listen(8080, () => {});
 
-app.get('/',(req,res)=>{
-    // console.log("Han sun raha hu bc")
-    res.send("han sun raha hu bc")
-})
-app.get('/name',(req,res)=>{
-  res.send({name:"ruddarm"});
-})
+app.get("/", (req, res) => {
+  // console.log("Han sun raha hu bc")
+  res.send("han sun raha hu bc");
+});
+app.get("/name", (req, res) => {
+  res.send({ name: "ruddarm" });
+});
