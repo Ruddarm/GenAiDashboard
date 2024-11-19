2ac4b29662fa23e75dabbe3cf5b08cfc5b0f2c36
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-19 13:39:08 +0530

    Testign deployment

--- backend/route/index.js
+++ backend/route/index.js
@@ -3,11 +3,25 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const DbOperation = require("../Db/MongoOperation");
 const participant = require("../Models/participants");
+
+const allowedOrigins = ['https://your-frontend.netlify.app'];
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  
+app.use(corsOptions)
 dotenv.config();
 const app = express();
 DbOperation();
 app.listen(8080, () => {});
 
 app.get('/',(req,res)=>{
-    console.log("Han sun raha hu bc")
+    // console.log("Han sun raha hu bc")
+    res.send("han sun raha hu bc")
 })
